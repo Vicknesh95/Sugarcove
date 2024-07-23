858ffbeeba4a8bb91c759163644c6b373f57150a
vicknesh95 <vickababa95@hotmail.com>
2024-07-23 12:11:52 +0800

    Completed controllers and routers for products, tested all endpoints on postman

--- BACKEND/src/controllers/products.js
+++ BACKEND/src/controllers/products.js
@@ -40,4 +40,50 @@ const addProducts = async (req, res) => {
   }
 };
 
-module.exports = { getProducts, addProducts };
+const updateProducts = async (req, res) => {
+  try {
+    const {
+      product_name,
+      product_type,
+      product_price,
+      product_description,
+      allergens,
+      image_url,
+      id,
+    } = req.body;
+
+    const updatedProducts = await pool.query(
+      "UPDATE products SET product_name = $1, product_type = $2, product_price = $3, product_description = $4, allergens = $5, image_url = $6 WHERE id = $7 RETURNING *",
+      [
+        product_name,
+        product_type,
+        product_price,
+        product_description,
+        allergens,
+        image_url,
+        id,
+      ]
+    );
+
+    res.json(updatedProducts.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+    res.json({ status: error, message: "error updating products" });
+  }
+};
+
+const deleteProducts = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const deleteProduct = await pool.query(
+      "DELETE FROM products WHERE id = $1",
+      [id]
+    );
+
+    res.json("product deleted successfully");
+  } catch (error) {
+    console.error(error.message);
+    res.json({ status: error, message: "error deleting products" });
+  }
+};
+module.exports = { getProducts, addProducts, updateProducts, deleteProducts };
--- BACKEND/src/routers/products.js
+++ BACKEND/src/routers/products.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts, addProducts } = require("../controllers/products");
+const {
+  getProducts,
+  addProducts,
+  updateProducts,
+  deleteProducts,
+} = require("../controllers/products");
 
 router.get("/product", getProducts);
 router.post("/product", addProducts);
+router.patch("/product", updateProducts);
+router.delete("/product", deleteProducts);
 module.exports = router;
