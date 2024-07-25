e110b828699ba4dbb61b0942d39ee1a330448d2d
vicknesh95 <vickababa95@hotmail.com>
2024-07-25 13:47:54 +0800

    middleware done, but have not put it into routes yet to protect endpoint, will put it in later on

---
+++ BACKEND/src/middleware/auth.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  if (!("authorization" in req.headers)) {
+    return res.status(400).json({ status: "error", msg: "token required" });
+  }
+
+  const token = req.headers["authorization"].replace("Bearer ", "");
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+      req.decoded = decoded;
+      next();
+    } catch (error) {
+      return res.status(401).json({ status: "error", msg: "not authorised" });
+    }
+  } else {
+    return res.status(403).json({ status: "error", msg: "forbidden" });
+  }
+};
+
+const authAdmin = (req, res, next) => {
+  if (!("authorization" in req.headers)) {
+    return res.status(400).json({ status: "error", msg: "token required" });
+  }
+
+  const token = req.headers["authorization"].replace("Bearer ", "");
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+
+      // additional role to create for admin account
+      // admin mods are in front end
+      if (decoded.role === "ADMIN") {
+        req.decoded = decoded;
+        next();
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      return res.status(403).json({ status: "error", msg: "not authorised" });
+    }
+  } else {
+    return res.status(403).json({ status: "error", msg: "forbidden" });
+  }
+};
+
+module.exports = { auth, authAdmin };
