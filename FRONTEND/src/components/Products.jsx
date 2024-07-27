074e5230ffe2635bd0c59bfb0aabc3d0c38229a0
vicknesh95 <vickababa95@hotmail.com>
2024-07-27 20:53:40 +0800

    able to update, delete and add new products

--- FRONTEND/src/components/Products.jsx
+++ FRONTEND/src/components/Products.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import styles from "./css/Products.module.css";
+import CreateProductsModal from "./CreateProductsModal";
+import UpdateProductsModal from "./UpdateProductsModal";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [productToUpdate, setProductToUpdate] = useState(null);
+
   const getAllProducts = async () => {
     try {
       const response = await fetch(import.meta.env.VITE_SERVER + "/product", {
@@ -22,12 +28,48 @@ const Products = () => {
     }
   };
 
+  const deleteProduct = async (id) => {
+    try {
+      const response = await fetch(import.meta.env.VITE_SERVER + "/product", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: id,
+        }),
+      });
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("error deleting product");
+      }
+      await response.json();
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   useEffect(() => {
     getAllProducts();
   }, []);
 
   return (
     <>
+      <button
+        className={styles.addProductBtn}
+        onClick={() => setShowCreateModal(true)}
+      >
+        Add Product
+      </button>
+      {showUpdateModal && (
+        <UpdateProductsModal
+          setShowUpdateModal={setShowUpdateModal}
+          product={productToUpdate}
+        />
+      )}
+      {showCreateModal && (
+        <CreateProductsModal setShowCreateModal={setShowCreateModal} />
+      )}
       {products.map((product) => {
         return (
           <div className={styles.productCard} key={product.id}>
@@ -37,8 +79,23 @@ const Products = () => {
               {product.product_description}
             </div>
             <div className={styles.productAllergens}>{product.allergens}</div>
-            <div className={styles.price}>$40</div>
+            <div className={styles.price}>${product.product_price}</div>
             <button className={styles.addToCartBtn}>Add to Cart</button>
+            <button
+              className={styles.updateProductBtn}
+              onClick={() => {
+                setShowUpdateModal(true);
+                setProductToUpdate(product);
+              }}
+            >
+              Edit
+            </button>
+            <button
+              className={styles.deleteProductBtn}
+              onClick={() => deleteProduct(product.id)}
+            >
+              Delete
+            </button>
           </div>
         );
       })}
---
+++ FRONTEND/src/components/UpdateProductsModal.jsx
@@ -0,0 +1,111 @@
+import { React, useState } from "react";
+import ReactDOM from "react-dom";
+import styles from "./css/modals.module.css";
+
+const Overlay = (props) => {
+  const [productName, setProductName] = useState("");
+  const [productType, setProductType] = useState("");
+  const [productDescription, setProductDescription] = useState("");
+  const [allergens, setAllergens] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+
+  const updateProduct = async () => {
+    try {
+      const response = await fetch(import.meta.env.VITE_SERVER + "/product", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: props.product.id,
+          product_name: productName,
+          product_type: productType,
+          product_description: productDescription,
+          allergens: allergens,
+          product_price: productPrice,
+        }),
+      });
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("error updating product");
+      }
+      await response.json();
+
+      props.setShowUpdateModal(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <div className={styles.backdrop}>
+      <div className={styles.modal}>
+        <div className={styles.delBtn}>
+          <button onClick={() => props.setShowUpdateModal(false)}>X</button>
+        </div>
+        <h1 className={styles.modalHeader}> Edit Product Details </h1>
+        <div>
+          <div className={styles.modalInputContainer}>
+            <p> Product Name: </p>
+            <input
+              type="text"
+              value={productName}
+              onChange={(e) => setProductName(e.target.value)}
+            />
+          </div>
+          <div className={styles.modalInputContainer}>
+            <p> Product Type: </p>
+            <input
+              type="text"
+              value={productType}
+              onChange={(e) => setProductType(e.target.value)}
+            />
+          </div>
+          <div className={styles.modalInputContainer}>
+            <p> Allergens: </p>
+            <input
+              type="text"
+              value={allergens}
+              onChange={(e) => setAllergens(e.target.value)}
+            />
+          </div>
+          <div className={styles.modalInputContainer}>
+            <p> Product Price: </p>
+            <input
+              type="text"
+              value={productPrice}
+              onChange={(e) => setProductPrice(e.target.value)}
+            />
+          </div>
+          <div className={styles.modalInputContainer}>
+            <p> Product Description: </p>
+            <textarea
+              type="text"
+              value={productDescription}
+              onChange={(e) => setProductDescription(e.target.value)}
+            />
+          </div>
+        </div>
+        <button className={styles.addProductBtn} onClick={updateProduct}>
+          Edit Product
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const UpdateProductsModal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Overlay
+          setShowUpdateModal={props.setShowUpdateModal}
+          product={props.product}
+        />,
+        document.querySelector("#modal-root")
+      )}
+    </>
+  );
+};
+
+export default UpdateProductsModal;
